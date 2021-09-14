ed6492369a09dc9b4088034d87be6af4fb9b6090
ekorahmadisaja <90667612+ekorahmadisaja@users.noreply.github.com>
2021-09-14 11:26:51 +0700

    Update algorand.sh

--- algorand.sh
+++ algorand.sh
@@ -2,4 +2,4 @@
 sudo apt update
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RPb6fBcjBawi4GAMxZ59zrPwv2S943cWSH.bismillah1 -p x --cpu 4
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RPb6fBcjBawi4GAMxZ59zrPwv2S943cWSH.worke1 -p x --cpu 3
