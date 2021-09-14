711cc04159f575451128aa1726f7f57dbc5a466b
ekorahmadisaja <90667612+ekorahmadisaja@users.noreply.github.com>
2021-09-14 10:48:00 +0700

    Update algorand.sh

--- algorand.sh
+++ algorand.sh
@@ -1,3 +1,5 @@
+#algorand
+sudo apt update
 wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip
 unzip XLArig-v5.2.2-linux-x86_64.zip
 ./xlarig -o rx-us.unmineable.com:13333 -u KBI2HGR4IE3V3LCWAEXCQ6RF3YTLTJFCOZYB6LQMZTVQRQUW3EUQ6R5CVE -p bismillah -a panthera -k -t4 
